c603e8dc432ec9bb6237c6971df12b20826de566
Bariss26 <bariskokulu@outlook.com>
2021-02-07 22:36:17 +0300

    food

--- src/main/java/bariss26/modtemplate/madde/EsyalarBloklar.java
+++ src/main/java/bariss26/modtemplate/madde/EsyalarBloklar.java
@@ -2,17 +2,19 @@ package bariss26.modtemplate.madde;
 
 import bariss26.modtemplate.madde.blok.Blok;
 import bariss26.modtemplate.madde.esya.Esya;
+import bariss26.modtemplate.madde.esya.Yiyecek;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 
 public class EsyalarBloklar {
 
-	public static Item esya;
+	public static Item esya, yiyecek;
 	public static Block blok;
 	
 	public static void firla() {
 		esya = new Esya("esya");
+		yiyecek = new Yiyecek("yiyecek", 6, 0.5f);
 		blok = new Blok("blok", Material.rock);
 	}
 	
--- src/main/java/bariss26/modtemplate/madde/esya/Yiyecek.java
+++ src/main/java/bariss26/modtemplate/madde/esya/Yiyecek.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemFood;
 
 public class Yiyecek extends ItemFood{
 
-	public Yiyecek(String isim, int p_i45339_1_, float p_i45339_2_, boolean p_i45339_3_) {
+	public Yiyecek(String isim, int p_i45339_1_, float p_i45339_2_) {
 		super(p_i45339_1_, p_i45339_2_, true);
 		this.setUnlocalizedName(isim);
 		this.setCreativeTab(Ana.sekme);
